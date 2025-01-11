23b2f344784164bc23f9044525efa5bb75820d69
Giovanni Prisco <gprisco01@gmail.com>
2025-01-11 23:18:48 +0100

    Added recursive gcd

--- gcd.cpp
+++ gcd.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+/* ----------Euclidean algorithm-----------
+    1.	Calculate the remainder of the division (large / small)
+    2.	Replace the larger number with the smaller number.
+    3.	Replace the smaller number with the remainder from step 1.
+    4.	Repeat steps 1-3 until the remainder is zero.
+    5.	The last non-zero remainder is the GCD.
+   ---------------------------------------- */
+
 void get_from_stdin_or(int *var, int fallback) {
   if (!(cin >> *var)) {
     cout << "You entered an invalid number." << endl;
@@ -30,6 +38,18 @@ int gcd(int low, int high) {
   return res;
 }
 
+int recursive_gcd(int *low, int *high) {
+  int res = *high % *low;
+
+  if (res <= 0) {
+    return *low;
+  }
+
+  *high = *low;
+  *low = res;
+  return recursive_gcd(low, high);
+}
+
 int get_number_from_stdin_and_validate(string msg) {
   int a = -1;
   while (a <= 0) {
@@ -49,7 +69,9 @@ int main() {
   cout << endl;
   int b = get_number_from_stdin_and_validate("Enter number b: ");
 
-  cout << gcd(a, b) << endl;
+  cout << "Greatest Common Divider: " << gcd(a, b) << endl;
+  cout << "Greatest Common Divider (recursively calculated): " << gcd(a, b)
+       << endl;
 
   return 0;
 }
